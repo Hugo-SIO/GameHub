373501b1fc041d171b6e18c41e23245d018772b6
Frifri26082005 <halmazanlaforet@gmail.com>
2026-08-23 17:37:44 +0200

    chore: initialisation du projet Spring Boot

---
+++ gamehub-backend/src/test/java/fr/hugoal/gamehubbackend/GamehubBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.hugoal.gamehubbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamehubBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
