18225ce09240ff7862558239905c8d8c162c1484
Frifri26082005 <halmazanlaforet@gmail.com>
2026-08-24 10:24:25 +0200

    feat(auth): ajout du repository User

---
+++ gamehub-backend/src/main/java/fr/hugoal/gamehubbackend/repositories/UserRepository.java
@@ -0,0 +1,17 @@
+package fr.hugoal.gamehubbackend.repositories;
+
+import fr.hugoal.gamehubbackend.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByUsername(String username);
+
+    Optional<User> findByEmail(String email);
+
+    boolean existsByUsername(String email);
+
+    boolean existsByEmail(String email);
+}
